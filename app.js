13da773f5a7ece31cb32ebaa169ff32518195e03
Strix007 <arbabashruff@gmail.com>
2021-08-23 20:42:29 +0530

    Styling Is Done And Body-Parser Is Working

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 const port = 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function (){
 
@@ -19,6 +20,14 @@ app.listen(port, function (){
 
 app.get("/", function(req, res){
    
-    res.send("Working");
+    res.sendFile(__dirname + "/signUp.html");
 
 });
+
+app.post("/", function (req, res){
+    var userFirstName = req.body.yourFirstName;
+    var userLastName = req.body.yourLastName;
+    var userEmailAddress = req.body.yourEmailAddress;
+    console.log(userFirstName, userLastName, userEmailAddress);
+    res.sendFile(__dirname + "/signUp.html")
+})
