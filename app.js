20d1eda065d262816da0086a4923314880544b29
Strix007 <arbabashruff@gmail.com>
2021-09-01 17:05:57 +0530

    Everything Works Now

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 
+// Mailchimp API Key
+
+// 4cfd253662a7abdc040d8932cd3ccca4-us5
+
+// Mailchimp List ID
+
+// e0fc551579
+
 const express = require("express");
 
 const https  = require("https");
@@ -29,5 +37,50 @@ app.post("/", function (req, res){
     var userLastName = req.body.yourLastName;
     var userEmailAddress = req.body.yourEmailAddress;
     console.log(userFirstName, userLastName, userEmailAddress);
-    res.sendFile(__dirname + "/signUp.html")
+
+    const data = {
+        members: [
+            {
+                email_address: userEmailAddress,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: userFirstName,
+                    LNAME: userLastName 
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url =  "https://us5.api.mailchimp.com/3.0/lists/e0fc551579";
+
+    const options = {
+        method: "POST",
+        auth: "arbab:4cfd253662a7abdc040d8932cd3ccca4-us5"
+
+    }
+
+    const request = https.request(url, options, function (response){
+
+        console.log(response.statusCode)
+        
+        response.on("data", function(data){
+        })
+
+        if (response.statusCode == 200){
+            res.sendFile(__dirname + "/suscess.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+    })
+
+    request.write(jsonData);
+    request.end();
+
+})
+
+app.post("/failure", function (req, res){
+    res.redirect("/");
 })
